74d87a593fc3dc23a98d892002327d99067b6a60
greenhornsr <srollins11279@gmail.com>
2020-01-27 16:41:43 -0800

    finished queue, confirmed working

--- queue_and_stack/dll_queue.py
+++ queue_and_stack/dll_queue.py
@@ -1,19 +1,31 @@
 import sys
 sys.path.append('../doubly_linked_list')
 from doubly_linked_list import DoublyLinkedList
+from doubly_linked_list import ListNode
 
+
+test = ListNode('testing')
 
 class Queue:
     def __init__(self):
         self.size = 0
         # Why is our DLL a good choice to store our elements?
-        # self.storage = ?
+            # we can easily access head, tail, next and prev nodes
+        self.storage = DoublyLinkedList()
 
     def enqueue(self, value):
-        pass
+        self.storage.add_to_tail(value)
+        # increment size
+        self.size += 1
+        
 
     def dequeue(self):
-        pass
+        if self.size > 0:
+            self.size -= 1
+            return self.storage.remove_from_head()
+        else: 
+            return None
 
     def len(self):
-        pass
+        return self.size
+
